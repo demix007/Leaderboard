7990cd6bbb41c9f27cba1bbc19f0f2ab21968ab5
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2022-10-28 03:16:36 +0100

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import appendRecentScores from './modules/recentScore.js';
+import appendRecentScores from './modules/recentScores.js';
 import appendAddYourScore from './modules/addNewScores.js';
 
 appendRecentScores();
