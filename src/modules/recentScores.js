2df91e22ab1470e79d82da979460d83144f24832
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-10 04:07:01 +0100

    ADD Stylings

--- src/modules/recentScores.js
+++ src/modules/recentScores.js
@@ -2,15 +2,15 @@ const LeaderBoard = document.getElementById('content');
 
 const recentScore = `
 <section class="score-board">
-            <div>
-                <h3>Recent scores</h3>
-                <button type="button" class="btn refresh-btn">Refresh</button>
-            </div>
-            <div class="score-wrap">
-                <ul class="scores" id="scores-board">
-                </ul>
-            </div>
-        </section>`;
+    <div>
+        <h3>Recent scores</h3>
+        <button type="button" class="btn refresh-btn">Refresh</button>
+    </div>
+    <div class="score-wrap">
+        <ul class="scores" id="scores-board">
+        </ul>
+    </div>
+</section>`;
 
 const appendRecentScores = () => {
   LeaderBoard.insertAdjacentHTML('afterbegin', recentScore);
